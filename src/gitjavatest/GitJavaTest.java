eedec83d293e8d54dfddf63ade4ffa6e5200a115
yannick <yannick@yannick-VirtualBox>
2016-06-23 16:16:14 +0200

    test 10022

---
+++ src/gitjavatest/GitJavaTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitjavatest;
+
+/**
+ *
+ * @author yannick
+ */
+public class GitJavaTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int t  =  0 ;
+    }
+    
+}
